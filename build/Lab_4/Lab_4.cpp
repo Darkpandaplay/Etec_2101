aadeb608548338beee7b0425cd2b992e7bad09b4
Darkpandaplay <52468163+Darkpandaplay@users.noreply.github.com>
2025-03-15 18:35:31 -0400

    progress

--- build/Lab_4/Lab_4.cpp
+++ build/Lab_4/Lab_4.cpp
@@ -1,20 +1,26 @@
-// Lab_4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
-#include <iostream>
-
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode({ 800,600 }), "Albro Lab4 Solution");
+    sf::CircleShape test_circle(100.0f);
+    test_circle.setFillColor(sf::Color::Green);
+    test_circle.setPosition(sf::Vector2f(10.0f, 10.0f));
+
+    while (window.isOpen())
+    {
+        //Iput-handling
+        while (const std::optional event = window.pollEvent())
+        {
+
+        }
+        //Drawing
+        window.clear();
+        window.draw(test_circle);
+        window.display();
+    }
+    return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
