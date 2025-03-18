ad32ac3931a27b835db6ae32b66a68169d7cd24a
Darkpandaplay <52468163+Darkpandaplay@users.noreply.github.com>
2025-03-18 14:36:24 -0400

    progess

--- build/Lab_4/Lab_4.cpp
+++ build/Lab_4/Lab_4.cpp
@@ -7,14 +7,22 @@ int main(int argc, char** argv)
     sf::RenderWindow window(sf::VideoMode({ 800,600 }), "Albro Lab4 Solution");
     sf::CircleShape test_circle(100.0f);
     test_circle.setFillColor(sf::Color::Green);
-    test_circle.setPosition(sf::Vector2f(10.0f, 10.0f));
+    float dy = 5.5;
+    test_circle.setPosition(sf::Vector2f(100.0f, 10.0f));
+    sf::FloatRect boundingBox = test_circle.getGlobalBounds();
+    sf::Vector2f point = sf::Vector2f(150.0f, 250.0f);
 
     while (window.isOpen())
     {
+        test_circle.move({ 0.f, 0.1f });
+        if (boundingBox.contains(point))
+        {
+            test_circle.move({ 0.f, -1.0f });
+        }
         //Iput-handling
         while (const std::optional event = window.pollEvent())
         {
-
+            
         }
         //Drawing
         window.clear();
