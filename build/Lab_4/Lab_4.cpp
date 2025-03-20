4992f72845af9abe34d198b55baa378351579761
Darkpandaplay <52468163+Darkpandaplay@users.noreply.github.com>
2025-03-19 23:06:58 -0400

    PROGRESS

--- build/Lab_4/Lab_4.cpp
+++ build/Lab_4/Lab_4.cpp
@@ -1,34 +1,103 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
+#include <array_list.h>
+#include <iostream>
+
+class TextCircle
+{
+private:
+	sf::Text my_text;
+	sf::CircleShape my_circle;
+
+
+public:
+	TextCircle(sf::Font& the_font) : my_text(the_font)  // We HAVE to use this syntax to pass
+	{								// the required parameter to my_text's constructor
+		//my_text(the_font);		// Nice thought, but too late.
+	}
+
+	void set_position(float x, float y)
+	{
+		my_text.setPosition(sf::Vector2f(x, y));
+		my_circle.setPosition(sf::Vector2f(x, y));
+	}
+
+	void set_text(std::string s)
+	{
+		my_text.setString(s);
+		// Adjust circle radius to fit??
+	}
+
+	void draw(sf::RenderWindow& rw)
+	{
+		rw.draw(my_text);
+		rw.draw(my_circle);
+	}
+};
+
 
 int main(int argc, char** argv)
 {
-    sf::RenderWindow window(sf::VideoMode({ 800,600 }), "Albro Lab4 Solution");
-    sf::CircleShape test_circle(100.0f);
-    test_circle.setFillColor(sf::Color::Green);
-    float dy = 5.5;
-    test_circle.setPosition(sf::Vector2f(100.0f, 10.0f));
-    sf::FloatRect boundingBox = test_circle.getGlobalBounds();
-    sf::Vector2f point = sf::Vector2f(150.0f, 250.0f);
+	
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "ALBRO'S Lab4 Solution");
+	sf::CircleShape test_circle(100.0f);
+	ssuds::ArrayList<float> circle_xlist;
+	ssuds::ArrayList<float> circle_ylist;
+	ssuds::ArrayList<std::string> circle_strings;
+	test_circle.setFillColor(sf::Color(200, 255, 100, 255));
+	test_circle.setPosition(sf::Vector2f(150.0f, 10.0f));
+	test_circle.setOrigin(sf::Vector2f(100.0f, 100.0f));
 
-    while (window.isOpen())
-    {
-        test_circle.move({ 0.f, 0.1f });
-        if (boundingBox.contains(point))
-        {
-            test_circle.move({ 0.f, -1.0f });
-        }
-        //Iput-handling
-        while (const std::optional event = window.pollEvent())
-        {
-            
-        }
-        //Drawing
-        window.clear();
-        window.draw(test_circle);
-        window.display();
-    }
-    return 0;
+	
 
+
+	
+	while (window.isOpen())
+	{
+		
+		while (const std::optional event = window.pollEvent())
+		{
+			
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close();
+			}
+			else if (const sf::Event::KeyPressed* keyEvt = event->getIf<sf::Event::KeyPressed>())
+			{
+				if (keyEvt->code == sf::Keyboard::Key::Escape)
+				{
+					window.close();
+				}
+			}
+			else if (const sf::Event::MouseButtonPressed* buttonEvt =
+				event->getIf<sf::Event::MouseButtonPressed>())
+			{
+				if (buttonEvt->button == sf::Mouse::Button::Left)
+				{
+				}
+			}
+
+		}
+
+		window.clear();
+		window.draw(test_circle);
+		////for (int i = 0; i < circle_xlist.size(); i++)
+		//{
+			//float x = circle_xlist[i];
+			//float y = circle_ylist[i];
+			//std::string s = circle_strings[i];
+		//}
+		//for (int i = 0; i < circle_ylist.size(); i++)
+		//{
+			//if (circle_ylist[i] >= 600)
+			//{
+				//circle_ylist.remove(i);
+				//circle_xlist.remove(i);
+			//}
+		//}
+
+		window.display();// Shows our drawings
+	}
+	return 0;
 }
