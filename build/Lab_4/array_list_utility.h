17e0a98d4f40d5b919f8fd22cd98c1947e1fb692
Darkpandaplay <52468163+Darkpandaplay@users.noreply.github.com>
2025-04-01 21:17:08 -0400

    Acending

---
+++ build/Lab_4/array_list_utility.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <array_list.h>
+#include <random>
+
+enum class sort_order { ASCENDING, DESCENDING };
+
+template <typename T>
+void sorto_bubble(ArrayList<T>& listo, sort_order order)
+{
+	int n = listo.size();
+	bool sorted;
+	if (order == sort_order::ASCENDING)
+	{
+		for (int i = 0; i < n - 1; i++)
+		{
+            if (order == sort_order::ASCENDING) {
+                for (int i = 0; i < n - 1; ++i) {
+                    bool sorted = true;
+                    for (int j = 0; j < n - 1 - i; ++j) { 
+                        if (listo[j] > listo[j + 1]) {
+                            std::swap(listo[j], listo[j + 1]); 
+                            sorted = false;
+                        }
+                    }
+                    if (sorted) break;
+                }
+            }
+		}
+	}
+};
